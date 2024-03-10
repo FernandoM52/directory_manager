46ee754ee7003057070253140b208307073a2215
FernandoM52 <fernandomartins.contato@outlook.com>
2024-03-10 20:14:14 -0300

    Refactor: styled components at Directory Page

--- src/pages/DirectoryPage/index.jsx
+++ src/pages/DirectoryPage/index.jsx
@@ -8,6 +8,7 @@ import { BASE_URL, useGetDirectories, useVerifyAccessToken } from "../../service
 import { DirectoryInput } from "../../components/CreateDirectoryForm/style";
 import DirectoryItem from "../../components/DirectoryItem";
 import useForm from "../../hooks/useForm";
+import { Content, CurrentDirectory, DirectoryEditForm, EditButton, Subdirectories, SubdirectoriesContainer } from "./style";
 
 function DirectoryPage() {
   const { id } = useParams();
@@ -87,72 +88,3 @@ function DirectoryPage() {
 }
 
 export default DirectoryPage;
-
-const Content = styled.div`
-  background-color: #202020;
-  display: flex;
-  width: 100%;
-  min-height: 100vh;
-  padding: 4%;
-  padding-top: 17%;
-`;
-
-const CurrentDirectory = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  gap: 10px;
-  width: 50%;
-  >p {
-    font-family: "Roboto", sans-serif;
-    font-size: 16px;
-    line-height: 20px;
-    color: #ffffff;
-  }
-`
-
-const DirectoryEditForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
-  width: 100%;
-`;
-
-const EditButton = styled.button`  
-  background-color: #fff;
-  color: #202020;
-  width: 220px;
-  border-radius: 6.25rem;
-  border-width: 0;
-  padding: 1.125rem 1.25rem 1.125rem 1rem;
-  pointer-events: ${(props) => (props.disabled ? "none" : "all")};
-  opacity: ${(props) => (props.disabled ? 0.7 : 1)};
-  font-family: "Roboto", sans-serif;
-  font-size: 16px;
-  cursor: pointer;
-  &:hover {
-    background-color: #e0e4ed;
-  }
-`
-
-const SubdirectoriesContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 50%;
-  gap: 10px;
-  >p {
-    font-family: "Roboto", sans-serif;
-    font-size: 16px;
-    line-height: 20px;
-    color: #ffffff;
-  }
-`;
-
-const Subdirectories = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 10px;
-`;
--- src/pages/DirectoryPage/style.js
+++ src/pages/DirectoryPage/style.js
@@ -1,2 +1,70 @@
 import styled from "styled-components";
 
+export const Content = styled.div`
+  background-color: #202020;
+  display: flex;
+  width: 100%;
+  min-height: 100vh;
+  padding: 4%;
+  padding-top: 17%;
+`;
+
+export const CurrentDirectory = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 10px;
+  width: 50%;
+  >p {
+    font-family: "Roboto", sans-serif;
+    font-size: 16px;
+    line-height: 20px;
+    color: #ffffff;
+  }
+`;
+
+export const DirectoryEditForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  width: 100%;
+`;
+
+export const EditButton = styled.button`  
+  background-color: #fff;
+  color: #202020;
+  width: 220px;
+  border-radius: 6.25rem;
+  border-width: 0;
+  padding: 1.125rem 1.25rem 1.125rem 1rem;
+  pointer-events: ${(props) => (props.disabled ? "none" : "all")};
+  opacity: ${(props) => (props.disabled ? 0.7 : 1)};
+  font-family: "Roboto", sans-serif;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover {
+    background-color: #e0e4ed;
+  }
+`;
+
+export const SubdirectoriesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  gap: 10px;
+  >p {
+    font-family: "Roboto", sans-serif;
+    font-size: 16px;
+    line-height: 20px;
+    color: #ffffff;
+  }
+`;
+
+export const Subdirectories = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+`;
